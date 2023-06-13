4ed449861a88917d27a506bdcb8ffeef3840fe41
Beka Deressa <bekabd116@gmail.com>
2023-06-13 17:09:40 -0400

    Activity Complete.

--- src/DebugActivity.java
+++ src/DebugActivity.java
@@ -1,35 +1,42 @@
+
 public class DebugActivity {
 
     public static int divideNumbers(int a, int b) {
         return a / b;
+
     }
 
     public static void main(String[] args) {
+        System.out.println("hello");
         int x = 5;
         int y = 0;
-
         System.out.println("Starting the program...");
 
-//            figure out how to "catch" the "divide by zero" error, don't just change the value on "y"
-        int result = divideNumbers(x, y)
-        System.out.println("Result: " + result);
+        // figure out how to "catch" the "divide by zero" error, don 't just change the value on "y"
+        try {
+            int result = divideNumbers(x, y);
 
-        String name = "John"
-        int length = name.length;
-        System.out.println("Length of name: " + length);
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero");
 
-        int numbers = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= numbers.length; i++) {
+            String name = "John";
+            int length = name.length();
+            System.out.println("Length of name: " + length);
+
+            int[] numbers = {1, 2, 3, 4, 5};
+            for (int number : numbers) {
 //                for loop should print one number each iteration
-            System.out.println(numbers);
+                System.out.println(number);
+            }
+
+            String[] fruits = {"Apple", "Banana", "Orange"};
+            for (String fruit : fruits) {
+                System.out.println(fruit);
+            }
+
+            System.out.println("End of the program.");
         }
 
-        String[] fruits = ["Apple", "Banana", "Orange"];
-        for (String fruit : fruit) {
-            System.out.println(fruit)
-        }
-
-        System.out.println('End of the program.');
     }
-
 }
